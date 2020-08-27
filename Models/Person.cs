344b8bed2cc94dba88fbd508eaf7f48c71efdef3
vitormoschetta <vitormoschetta@gmail.com>
2020-08-26 22:28:25 -0400

    Add project

---
+++ Models/Person.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PdfReport.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
